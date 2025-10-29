b625183a3853bab25b47a3efb8427f76ab275c36
joric <joric@users.noreply.github.com>
2025-10-30 01:55:17 +0500

    updated files

--- build.cmd
+++ build.cmd
@@ -10,3 +10,5 @@ set chunk=69
 copy %src%\pakchunk%chunk%-Windows.pak  %dest%\%modname%.pak
 copy %src%\pakchunk%chunk%-Windows.ucas %dest%\%modname%.ucas
 copy %src%\pakchunk%chunk%-Windows.utoc %dest%\%modname%.utoc
+
+copy %dest%\%modname%.* dist\Supraworld\Content\Paks\LogicMods
